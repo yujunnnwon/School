bace71f10494db46ded3cf650b0ba2efeb95cc2b
yujunnnwon <97663911+yujunnnwon@users.noreply.github.com>
2022-03-21 00:48:25 +0900

    Add files via upload

---
+++ ECE4326/Week3/3_up_sampling.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("fig0.PNG")
+H, W, C = img.shape
+print(H, W, C)
+# Up Sampling
+UP = 2
+
+# NN(Nearest Neiborhood) Interpolation
+
+# Before Interpolation
+NN = cv2.resize(img, dsize=(W*UP, H*UP), interpolation=cv2.INTER_NEAREST)   # interpolation을 NN으로 설정
+
+UPSAM = np.zeros((H*UP, W*UP, C))                                           # (H*UP, W*UP, C)의 3차원 matrix 생성
+# UPSAM(zero matrix)의 픽셀에 img의 픽셀을 mapping
+for i in range(H):
+    for j in range(W):
+        UPSAM[2*i, 2*j, :] = img[i, j, :]
+
+UPSAM = UPSAM.astype(np.uint8)                                              # UPSAM의 matrix를 형변환
+cv2.imshow("Before_interpolation", UPSAM)
+
+# After Interpolation
+for i in range(H):
+    for j in range(W):
+        UPSAM[2*i+1, 2*j+1, :] = img[i, j, :]
+        UPSAM[2*i, 2*j+1, :] = img[i, j, :]
+        UPSAM[2*i+1, 2*j, :] = img[i, j, :]
+
+UPSAM = UPSAM.astype(np.uint8)
+cv2.imshow("After_interpolation", UPSAM)
+cv2.waitKey()
+cv2.destroyAllWindows()
