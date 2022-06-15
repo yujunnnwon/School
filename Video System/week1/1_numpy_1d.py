52d771b47194d78a31ca4239bc52bcc19bbb4452
yujunnnwon <97663911+yujunnnwon@users.noreply.github.com>
2022-06-15 19:11:22 +0900

    Update 1_numpy_1d.py

--- Video System/week1/1_numpy_1d.py
+++ Video System/week1/1_numpy_1d.py
@@ -4,16 +4,16 @@ import numpy as np
 a = [1, 2, 3, 4, 5]
 b = np.array(a)
 
-print(type(a))          # <class 'list'>
-print(type(b))          # <class 'numpy.ndarray'>
-print("a = ", a)        # a = [1, 2, 3, 4, 5]
-print("b = ", b)        # b = [1 2 3 4 5]
+print(type(a))         # <class 'list'>
+print(type(b))         # <class 'numpy.ndarray'>
+print("a =", a)        # a = [1, 2, 3, 4, 5]
+print("b =", b)        # b = [1 2 3 4 5]
 
 # 1D Array의 연산: 동일한 Dimension의 성분별 연산
 a = np.array([1, 2, 3, 4, 5])
 b = np.array([6, 7, 8, 9, 10])
 
-print("a+b = ", a+b)
-print("a-b = ", a-b)
-print("a*b = ", a*b)
-print("a/b = ", a/b)
+print("a+b =", a+b)
+print("a-b =", a-b)
+print("a*b =", a*b)
+print("a/b =", a/b)
