523d655c00d876e4af2b2d06b7d709026b86e17a
yujunnnwon <97663911+yujunnnwon@users.noreply.github.com>
2022-06-15 19:12:09 +0900

    Update 2_numpy_2d.py

--- Video System/week1/2_numpy_2d.py
+++ Video System/week1/2_numpy_2d.py
@@ -1,4 +1,3 @@
-from tokenize import _all_string_prefixes
 import numpy as np
 
 # 2D Array
@@ -12,12 +11,12 @@ print(type(b))              # <class 'list'>
 print(type(arr_a))          # <class 'numpy.ndarray'>
 print(type(arr_b))          # <class 'numpy.ndarray'>
 
-print("arr_a = ", arr_a)
-# arr_a =  [[1 2 3]
-#           [4 5 6]
-#           [7 8 9]]
+print("arr_a =", arr_a)
+# arr_a = [[1 2 3]
+#          [4 5 6]
+#          [7 8 9]]
 
-print("arr_b = ", arr_b)
-# arr_b =  [[7 8 9]
-#           [4 5 6]
-#           [1 2 3]]
+print("arr_b =", arr_b)
+# arr_b = [[7 8 9]
+#          [4 5 6]
+#          [1 2 3]]
